1f0c2a3f492e3b5b5d34e980a01a4e85b1688265
mona52 <mona52@list.ru>
2021-03-22 23:06:54 +0300

    initial commit for junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,29 +1,29 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAmountBelowBoundary() {
         int amount = 600;
         assertEquals(service.remain(amount), 400);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAmountUpperBoundary() {
         int amount = 1800;
         assertEquals(service.remain(amount), 200);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAmountEqualBoundary() {
         int amount = 1000;
         assertEquals(service.remain(amount), 0);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldAmountIfZero() {
         int amount = 0;
         assertEquals(service.remain(amount), 1000);

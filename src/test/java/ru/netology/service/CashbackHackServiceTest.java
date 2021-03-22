fe9e45b8ffb2cdd98c794553edb041ea66c0ebd5
mona52 <mona52@list.ru>
2021-03-22 23:40:31 +0300

    add test for JUP5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -29,4 +29,28 @@ public class CashbackHackServiceTest {
         assertEquals(service.remain(amount), 1000);
     }
 
+    @org.junit.jupiter.api.Test
+    public void shouldAmountBelowBoundaryForJ5() {
+        int amount = 600;
+        assertEquals(400,service.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldAmountUpperBoundaryForJ5() {
+        int amount = 1800;
+        assertEquals(200,service.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldAmountEqualBoundaryForJ5() {
+        int amount = 1000;
+        assertEquals(0,service.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldAmountIfZeroForJ5() {
+        int amount = 0;
+        assertEquals(1000, service.remain(amount));
+    }
+
 }
